58fa42e9d838c0d48bf06c3a44abc6277a4bfea3
NAVEEN <naveen@sais-MacBook-Air.local>
2021-05-29 09:09:52 +0530

    added coupon enum

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,17 @@
 class Coupon < ApplicationRecord
 	has_many :coupon_redemptions
+    def coupon_types_enum
+        [ 'amount', 'percentage'  ]
+    end
+
+    rails_admin do 
+		edit do 
+			field :coupon_type, :enum do
+				render do
+					bindings[:form].select( "coupon_type", bindings[:object].coupon_types_enum, {})
+				end
+			end
+			include_all_fields
+		end
+	end
 end
