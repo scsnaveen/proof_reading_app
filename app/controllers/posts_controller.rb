0f65b738989ad2276c786b99b4fcab01c6da3c90
NAVEEN <naveen@sais-MacBook-Air.local>
2021-06-02 12:15:15 +0530

    modified user accept

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,14 +71,11 @@ class PostsController < ApplicationController
 		# getting count of different words
 		@post.words =diff.count
 		p @post.words.inspect
-		# checking if coupon is present  
-		if @user_payment.discount_id.present?
-			@coupon = Coupon.find(@user_payment.discount_id) 
-			@coupon_redemption = CouponRedemption.where("coupon_id =? AND user_id=?", @coupon.id,current_user.id).first
 			loop do 
 				@payment_reference = ( "success" + [*(0..9)].sample(10).join.to_s )
 				break @payment_reference unless Payment.exists?(reference_id: @payment_reference)
 			end
+
 			# user return payment
 			@payment                  = Payment.new()
 			@payment.amount           = @post.words * PaymentCharge.first.per_word_amount.to_f
@@ -86,30 +83,33 @@ class PostsController < ApplicationController
 			@payment.status           = "refund"
 			@payment.user_id          = current_user.id
 			@payment.post_id          = @post.id
-			@payment.discount_id      = @coupon.id
-			@payment.payment_type     = "debited"
-			# if coupon type is percentage
-			if @coupon.coupon_type == "percentage"
-				# if payment amount is greater than coupon amount
-				if @payment.amount.to_f > @coupon.amount.to_f
-					percentage_amount = @payment.amount.to_f/100.0 * @coupon.percentage.to_f
-					# if coupon percentage amount is greater than coupon limit amount
-					if percentage_amount > @coupon.amount.to_f
-						@payment.discount_amount  = @coupon.amount.to_f
+			# checking if coupon is present  
+				if @user_payment.discount_id.present?
+					@coupon = Coupon.find(@user_payment.discount_id) 
+					@coupon_redemption = CouponRedemption.where("coupon_id =? AND user_id=?", @coupon.id,current_user.id).first
+					@payment.discount_id      = @coupon.id
+					@payment.payment_type     = "debited"
+					# if coupon type is percentage
+					if @coupon.coupon_type == "percentage"
+							percentage_amount = @payment.amount.to_f/100.0 * @coupon.percentage.to_f
+							# if coupon percentage amount is greater than coupon limit amount
+							if percentage_amount.to_f > @coupon.amount.to_f
+								@payment.discount_amount  = @coupon.amount.to_f
+							else
+								@payment.discount_amount = percentage_amount.to_f
+							end
 					else
-						@payment.discount_amount = percentage_amount.to_f
+						# if coupon type is amount 
+						if  @payment.amount.to_f > @coupon.amount.to_f
+							@payment.discount_amount  = @coupon.amount.to_f
+						else
+							@payment.discount_amount  = @payment.amount.to_f
+						end
 					end
-				else
-					@payment.discount_amount = @payment.amount.to_f
+					@user_return_amount        = @user_payment.paid_amount.to_f - @payment.amount.to_f + @payment.discount_amount.to_f
+				else 
+					@user_return_amount        = @user_payment.paid_amount.to_f - @payment.amount.to_f 
 				end
-			else
-				if  @payment.amount.to_f > @coupon.amount.to_f
-					@payment.discount_amount  = @coupon.amount.to_f
-				else
-					@payment.discount_amount  = @payment.amount.to_f
-				end
-			end
-			@user_return_amount        = @user_payment.paid_amount.to_f - @payment.amount.to_f + @payment.discount_amount.to_f
 			@payment.amount            = @user_payment.paid_amount.to_f - @user_return_amount.to_f
 			@payment.paid_amount       = @user_return_amount.to_f
 			@balance_amount            = @user_payment.paid_amount.to_f - @user_return_amount.to_f
@@ -125,27 +125,14 @@ class PostsController < ApplicationController
 			end
 			@payment.save
 
-			# user paid amount
-			@payment1                  = Payment.new()
-			@payment1.amount           = @payment.amount
-			@payment1.reference_id     = @payment_reference
-			@payment1.status           = "completed"
-			@payment1.user_id          = current_user.id
-			@payment1.post_id          = @post.id
-			@payment1.discount_id      = @coupon.id
-			@payment1.payment_type     = "credited"
-			@payment1.discount_amount   = @payment.discount_amount
-			@payment1.paid_amount       = @payment.amount
-			@payment1.save
-
 			# payment for super admin commission to successful proof reading 
 			@super_admin_payment               = Payment.new()
 			@super_admin_payment.admin_id      = Admin.find_by(:role=>"Super Admin").id
-			@super_admin_payment.amount        = (PaymentCharge.first.commission_percentage.to_f / 100 * @payment.amount.to_f).round(2)
+			@super_admin_payment.amount        = PaymentCharge.first.commission_percentage.to_f/100 * @payment.amount.to_f.round(2)
 			@super_admin_payment.reference_id  = @payment_reference
 			@super_admin_payment.status        = "completed"
 			@super_admin_payment.post_id       = @post.id
-			@super_admin_payment.paid_amount   = @super_admin_payment.amount
+			@super_admin_payment.paid_amount   = @super_admin_payment.amount.to_f
 			@super_admin_wallet                = Admin.find_by(:role=>"Super Admin").wallet
 			@super_admin_wallet.transaction do
 					@super_admin_wallet.with_lock do 
@@ -182,10 +169,6 @@ class PostsController < ApplicationController
 			@request.status="completed"
 			@request.save
 			redirect_to posts_index_path,notice: "Thank you ,Please visit again"
-		else
-			flash[:alert]="Enter valid coupon code"
-			redirect_to payments_new_path(:id=>@post.id)	
-		end
 	end
 
 	# if user rejected the updated text
